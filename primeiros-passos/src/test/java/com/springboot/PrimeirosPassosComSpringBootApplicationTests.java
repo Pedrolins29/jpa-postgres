b30ba5a1bb17525ca0ec996b621553caa6bf3dbf
PEDRO LINS <pedro.hcl29@gmail.com>
2024-05-21 16:34:19 -0300

    primeiro commit

---
+++ primeiros-passos/src/test/java/com/springboot/PrimeirosPassosComSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirosPassosComSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
